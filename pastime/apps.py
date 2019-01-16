52e0aba65ce3df0ae68a87298c3a3f5ec7c0dfef
Kevine Musoni <kevinenicky@gmail.com>
2019-01-16 08:52:09 +0300

    structure

---
+++ pastime/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PastimeConfig(AppConfig):
+    name = 'pastime'
---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kevine/Desktop/pythonFiles/pastimeapp/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
